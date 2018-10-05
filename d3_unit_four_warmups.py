7bf2830e117bde77e78080bc29f52fce204ca852
Mark <mark@iMac2.local>
2018-10-05 13:33:25 -0400

    elif statements

---
+++ d3_unit_four_warmups.py
@@ -0,0 +1,10 @@
+grade = int(input("what grade are you in?"))
+
+if grade >= 9 and grade <= 12:
+    print("you are in high school")
+
+elif grade <= 8 and grade >= 6:
+    print("you are in middle school")
+
+else:
+    print("you are in lower school")

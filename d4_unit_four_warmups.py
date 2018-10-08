0359bbe123c923fed9c14d8b2d483a27803192b1
Mark <mark@iMac2.local>
2018-10-08 14:27:32 -0400

    if, elif, and else

---
+++ d4_unit_four_warmups.py
@@ -0,0 +1,7 @@
+age = int(input("what is your age?"))
+if age <= 12 and age >= 3:
+    print("your price is $8")
+elif age >= 13:
+    print("your price would be $10")
+elif age <= 2:
+    print("your price would be $0")
